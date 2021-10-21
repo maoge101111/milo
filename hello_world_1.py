96444da2cfc325628941a966cf7a67bb3e29f77c
maoge101111 <92921237+maoge101111@users.noreply.github.com>
2021-10-21 18:17:31 +0800

    Add files via upload

---
+++ hello_world_1.py
@@ -0,0 +1,43 @@
+print("hello python world!")
+message="hello python world!"
+print(message)
+
+message="hello python world!"
+print(message)
+
+message="hello python Crash Course world!"
+print(message)
+
+message="hello python Crash Course world!"
+print(message)
+
+"this is a string."
+'this is also a string'
+'I told my friend, "python is my favorite language!"'
+"The languoge 'python' is named after monty python, not the snake."
+"One of python's strengthe is its diverse and supportive community."
+name="ada lovelace"
+print(name.title())
+name="Ada lovelace"
+print(name.upper())
+print(name.lower())
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name} {last_name}"
+print(full_name)
+
+first_name="ada"
+last_name="lovelace"
+full_name=f"{first_name} {last_name}"
+print(f"hello, {full_name.title()}!")
+
+first_nane="ada"
+last_name="lovelace"
+fill_name=f"{first_name} {last_name}"
+message= f"hello, {full_name.title()}!"
+print(message) 
+
+print("python")
+print("\tpython")
+print("languages:\npython\nc\njavascript")
+
